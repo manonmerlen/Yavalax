765a9e2ff28b7ffc0c103abb25a5c74fa148c570
Manon Merlen <manonmerlen59@gmail.com>
2018-06-22 13:25:42 +0200

    final IA

--- test/test_yavalax_random.mjs
+++ test/test_yavalax_random.mjs
@@ -1,24 +1,39 @@
 import lib from '../lib/openxum-core/openxum';
 
-let e = new lib.OpenXum.Yavalax.Engine(lib.OpenXum.Yavalax.GameType.STANDARD, lib.OpenXum.Yavalax.Color.WHITE);
-let p1 = new lib.OpenXum.RandomPlayer(lib.OpenXum.Yavalax.Color.WHITE, e);
+
+
+let white_win = 0;
+let blue_win = 0;
+
+for (let i = 0; i < 10; ++i) {
+  let e = new lib.OpenXum.Yavalax.Engine(lib.OpenXum.Yavalax.GameType.STANDARD, lib.OpenXum.Yavalax.Color.WHITE);
+//let p1 = new lib.OpenXum.RandomPlayer(lib.OpenXum.Yavalax.Color.WHITE, e);
 //let p1 = new lib.OpenXum.Yavalax.IA.IAYavalaxPlayer(lib.OpenXum.Yavalax.Color.WHITE, e);
-//let p1 = new lib.OpenXum.MCTSPlayer(lib.OpenXum.Yavalax.Color.WHITE, e);
+let p1 = new lib.OpenXum.MCTSPlayer(lib.OpenXum.Yavalax.Color.WHITE, e);
 //let p2 = new lib.OpenXum.RandomPlayer(lib.OpenXum.Yavalax.Color.BLUE, e);
 //let p2 = new lib.OpenXum.MCTSPlayer(lib.OpenXum.Yavalax.Color.BLUE, e);
-let p2 = new lib.OpenXum.Yavalax.IA.IAYavalaxPlayer(lib.OpenXum.Yavalax.Color.BLUE, e);
-let p = p1;
-let moves = [];
+  let p2 = new lib.OpenXum.Yavalax.IA.IAYavalaxPlayer(lib.OpenXum.Yavalax.Color.BLUE, e);
+  let p = p1;
+  let moves = [];
 
-while (!e.is_finished()) {
+  while (!e.is_finished()) {
     let move = p.move();
 
     moves.push(move);
     e.move(move);
     p = p === p1 ? p2 : p1;
-}
+  }
 
-console.log("Winner is " + (e.winner_is() === lib.OpenXum.Yavalax.Color.WHITE ? "WHITE" : "BLUE"));
-for (let index = 0; index < moves.length; ++index) {
-    console.log(moves[index].to_string());
+  console.log("Winner is " + (e.winner_is() === lib.OpenXum.Yavalax.Color.WHITE ? "WHITE" : "BLUE"));
+  for (let index = 0; index < moves.length; ++index) {
+   //  console.log(moves[index].to_string());
+  }
+  if (e.winner_is() === lib.OpenXum.Yavalax.Color.WHITE) {
+    white_win++;
+  } else {
+    blue_win++;
+  }
 }
+console.log("White wins: " + white_win);
+console.log("Blue wins: " + blue_win);
+
